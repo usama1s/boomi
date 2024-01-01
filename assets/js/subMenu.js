8703afa7edce11c69ccbd304f296c9174644c8f6
Usama Munir <usama_1s@hotmail.com>
2024-01-02 01:09:25 +0500

    nav complete, submenu complete, dark mode complete, sidebar complete

---
+++ assets/js/subMenu.js
@@ -0,0 +1,18 @@
+$(".nav-btn-submenu").on("DOMSubtreeModified", function () {});
+
+$("#prompts, #attributes, #closeAlll").change((event) => {
+  var subMenu = $(event.target).siblings(".nav-btn-submenu");
+  if (subMenu.length > 0) {
+    var height = $(subMenu).children("div").innerHeight();
+
+    var totalHeight = 0;
+    $(subMenu)
+      .children("div")
+      .children()
+      .each(function () {
+        totalHeight = totalHeight + $(this).outerHeight(true);
+      });
+
+    $(subMenu).css("height", totalHeight + 40);
+  }
+});
--- assets/js/toggleModes.js
+++ assets/js/toggleModes.js
@@ -5,3 +5,11 @@ $("#light-dark-mode").on("input", function() {
         $("body").removeClass("mode-dark");
     }
  });
+
+ $(document).ready(function() {
+    if ($("#light-dark-mode").is(":checked")) {
+        $("body").addClass("mode-dark");
+    }else{
+        $("body").removeClass("mode-dark");
+    }
+});
