d5a504724301da4454cb3296c6b911e4da152dc8
Usama Munir <usama_1s@hotmail.com>
2024-01-05 21:31:01 +0500

    on load dark stays if selected

--- assets/js/toggleModes.js
+++ assets/js/toggleModes.js
@@ -1,15 +1,32 @@
 $("#light-dark-mode").on("input", function() {
+    
     if ($(this).is(":checked")) {
+        localStorage.setItem("dark-mode", true);
         $("body").addClass("mode-dark");
     }else{
+        localStorage.setItem("dark-mode", false);
         $("body").removeClass("mode-dark");
     }
  });
 
  $(document).ready(function() {
-    if ($("#light-dark-mode").is(":checked")) {
-        $("body").addClass("mode-dark");
+    var darkMode = localStorage.getItem("dark-mode");
+    console.log("load "+darkMode);
+    if (darkMode!==null) {
+        if (darkMode == "true") {
+            console.log("yes");
+            $( "#light-dark-mode").prop("checked", true ).trigger("input"); 
+        }else{
+            console.log("no");
+            $( "#light-dark-mode").prop("checked", false ).trigger("input");
+        }
+        
     }else{
-        $("body").removeClass("mode-dark");
+        if ($("#light-dark-mode").is(":checked")) {
+            $("body").addClass("mode-dark");
+        }else{
+            $("body").removeClass("mode-dark");
+        }
     }
+    
 });
